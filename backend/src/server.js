4247baaa2eb34fcdec40bba84844ab20bbe23bea
user.name like : "login.anujyadav@gmail.com <login.anujyadav@gmail.com>
2025-12-04 17:00:23 +0530

    auth and db sync added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,9 @@ import express from "express"
 import {ENV} from "./lib/env.js"
 import path from "path"
 import { connectDB } from "./lib/db.js"
+import cors from "cors"
+import {serve} from "inngest/express"
+import { inngest } from "./lib/inngest.js"
 
 
 // console.log(ENV.DB_URL); //Undefined if not config dotenv
@@ -9,6 +12,14 @@ import { connectDB } from "./lib/db.js"
 
 const app = express()
 const __dirname = path.resolve()
+
+//middleware
+app.use(express.json())
+//credentials->Sever allows the browser to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+app.use("/api/inngest",serve({client:inngest, functions}))
+
 
 app.get("/health", (req,res) => {
     res.status(200).json({"message":"Api is up and running"})
